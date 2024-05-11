b827ab63bac66ea145aa1db4fc3e4cb8f9e2611b
ldavidtl <torres_d44@yahoo.es>
2024-05-10 21:21:12 -0600

    initial commit

---
+++ src/test/java/com/producto/prd/PrdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.producto.prd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
